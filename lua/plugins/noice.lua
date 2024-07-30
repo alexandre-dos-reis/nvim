b250cdeae9182bc94febbc23ce526853e5e6b736
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2024-07-30 13:32:31 +0200

    noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -12,6 +12,14 @@ return {
     routes = {
       {
         filter = {
+          event = "msg_show",
+          kind = "",
+          find = "written",
+        },
+        opts = { skip = true },
+      },
+      {
+        filter = {
           event = "notify",
           find = "No information available",
         },
