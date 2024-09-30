88efccdb22664f79296ba59fddf9a04a725caddc
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2024-09-30 16:39:38 +0200

    git removed unused git plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -12,14 +12,4 @@ return {
       })
     end,
   },
-  {
-    "NeogitOrg/neogit",
-    branch = "master",
-    config = true,
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- required
-      "sindrets/diffview.nvim", -- optional - Diff integration
-      "nvim-telescope/telescope.nvim", -- optional
-    },
-  },
 }
