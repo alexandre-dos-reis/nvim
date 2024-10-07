865add13adc3442da9ae2b867d9aae679daf2a74
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2024-10-07 10:33:20 +0200

    fix noice config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,37 +1,36 @@
 return {
   "folke/noice.nvim",
   opts = function(_, opts)
-    table.insert(opts, {
-      {
-        views = {
-          cmdline_popup = {
-            position = {
-              row = "40%",
-              height = "50%",
-            },
-          },
-        },
-        routes = {
-          {
-            filter = {
-              event = "msg_show",
-              kind = "",
-              find = "written",
-            },
-            opts = { skip = true },
-          },
-          {
-            filter = {
-              event = "notify",
-              find = "No information available",
-            },
-            opts = { skip = true },
-          },
-        },
+    -- Remove message on save
+    table.insert(opts.routes, {
+      filter = {
+        event = "msg_show",
+        kind = "",
+        find = "written",
       },
+      opts = { skip = true },
     })
 
-    opts.presets = {
+    -- Remove message when no info is available
+    table.insert(opts.routes, {
+      filter = {
+        event = "notify",
+        find = "No information available",
+      },
+      opts = { skip = true },
+    })
+
+    -- Place the cmdline popup at the center
+    opts.views = {
+      cmdline_popup = {
+        position = {
+          row = "40%",
+          height = "50%",
+        },
+      },
+    }
+
+    opts.preset = {
       lsp_doc_border = true,
       bottom_search = false,
       inc_rename = true,
