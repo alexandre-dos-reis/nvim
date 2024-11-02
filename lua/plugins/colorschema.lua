f9d9b169484740ca968c18eaa2a725369181a721
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2024-11-02 22:54:32 +0100

    colorscheme, telescope and telescope-file-browser

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -4,8 +4,38 @@ return {
   priority = 1000,
   opts = {
     transparent = true,
+    on_highlights = function(hl, c)
+      hl.TelescopeNormal = {
+        bg = c.bg_dark,
+        fg = c.fg_dark,
+      }
+      hl.TelescopeBorder = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+      hl.TelescopePromptNormal = {
+        bg = c.bg_dark,
+      }
+      hl.TelescopePromptBorder = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+      hl.TelescopePromptTitle = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+      hl.TelescopePreviewTitle = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+      hl.TelescopeResultsTitle = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+    end,
   },
-  config = function()
+  config = function(_, opts)
+    require("solarized-osaka").setup(opts)
     vim.cmd.colorscheme("solarized-osaka")
 
     -- Remove bg color for error, warn, info and hint !
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -28,7 +28,6 @@ return {
       require("telescope").setup({
         extensions = {
           file_browser = {
-            theme = "ivy",
             -- disables netrw and use telescope-file-browser in its place
             initial_mode = "normal",
             hijack_netrw = true,
