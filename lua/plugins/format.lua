1c5e93b1b62bc6b1bc0a5045302d73eaa1d75730
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2024-11-14 15:44:08 +0100

    format sql file with pg_format

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -31,6 +31,7 @@ return {
         },
         sh = { "beautysh" },
         rust = { "rustfmt" },
+        sql = { "pg_format" },
       },
       default_format_opts = {
         lsp_format = "fallback",
