ed63405b3a90996db380d61801e39425ef85d3eb
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2024-12-13 13:13:34 +0100

    lazy update

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,7 +3,7 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
-    -- bigfile = { enabled = true },
+    bigfile = { enabled = true },
     -- notifier = { enabled = true },
     -- quickfile = { enabled = true },
     -- statuscolumn = { enabled = true },
