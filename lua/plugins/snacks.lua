9f278890c6f2c601caaacba84a013adc26983da2
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2025-11-12 20:32:27 +0100

    more straigh

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -36,31 +36,32 @@ return {
           end
 
           -- For each buffer
-          Snacks.bufdelete.delete(function(b)
-            if
-              -- Don't delete the current buffer
-              buf == b
-              -- Don't delete buffer present in other window
-              or win_buffers[b]
-            then
-              return false
-            end
+          Snacks.bufdelete.delete({
+            force = true,
+            filter = function(b)
+              if
+                -- Don't delete the current buffer
+                buf == b
+                -- Don't delete buffer present in other window
+                or win_buffers[b]
+              then
+                return false
+              end
 
-            -- Set path/name to empty name buffer
-            if vim.api.nvim_buf_get_name(b) == "" then
-              local id = string.format("%08x", math.random(0, 0xffffffff))
-              local path = vim.fs.joinpath(vim.fn.getcwd(), "unsaved-file-" .. id)
-              vim.api.nvim_buf_set_name(b, path)
-            end
+              -- Delete orphan code buffer
+              if vim.api.nvim_buf_get_name(b) == "" then
+                return true
+              end
 
-            -- Autosave before deleting
-            if vim.bo[b].modified then
-              vim.api.nvim_buf_call(b, vim.cmd.write)
-            end
+              -- Autosave before deleting
+              if vim.bo[b].modified then
+                vim.api.nvim_buf_call(b, vim.cmd.write)
+              end
 
-            -- Delete
-            return true
-          end)
+              -- Delete
+              return true
+            end,
+          })
         end
       end,
     })
