1bdabefe66269fdd638686c3d94c3229d2d97f85
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2025-11-13 22:08:39 +0100

    resolve linter based on config file

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,9 +1,17 @@
 local linters = { eslint = "eslint_d", oxlint = "oxlint" }
 
--- TODO: For the moment nvim-lint run every linters for one ft.
--- Try to find the default linter config file and use it, then the others, ...
--- and pass it to `try_lint`
--- See the linters branch
+local config_files_by_linters = {
+  [linters.eslint] = {
+    "eslint.config.js",
+    "eslint.config.mjs",
+    "eslint.config.cjs",
+    "eslint.config.ts",
+    "eslint.config.mts",
+    "eslint.config.cts",
+  },
+  [linters.oxlint] = { ".oxlintrc.json" },
+}
+
 local js_linters = { linters.eslint, linters.oxlint }
 
 local linters_by_ft = {
@@ -12,6 +20,23 @@ local linters_by_ft = {
   javascriptreact = js_linters,
   typescriptreact = js_linters,
 }
+
+-- This resolve the linter name based on the project config file
+local resolve_linter = function(buffer, file)
+  local ft = vim.bo[buffer].ft
+
+  if ft ~= "" or file ~= "" then
+    local linters_table = linters_by_ft[ft]
+
+    for _, _linter in pairs(linters_table) do
+      local found = vim.fs.find(config_files_by_linters[_linter], { upward = true, path = file })
+      if not vim.tbl_isempty(found) then
+        return _linter
+      end
+    end
+  end
+  return nil
+end
 
 return {
   "mfussenegger/nvim-lint",
@@ -25,25 +50,33 @@ return {
 
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
-      callback = function()
-        lint.try_lint()
+      callback = function(e)
+        local ft = vim.bo[e.buf].ft
+
+        if linters_by_ft[ft] == nil then
+          return
+        end
+
+        lint.try_lint(resolve_linter(e.buf, e.file))
       end,
     })
 
-    local lint_progress = function()
-      local running_linters = require("lint").get_running()
-      if #running_linters == 0 then
-        return "󰦕  No linters running."
-      end
-      return "󱉶  linters running: " .. table.concat(linters, ", ")
-    end
-
     vim.keymap.set("n", "<leader>lt", function()
-      lint.try_lint()
+      local buf = vim.api.nvim_get_current_buf()
+      local linter_resolved = resolve_linter(buf, vim.api.nvim_buf_get_name(buf))
+
+      if linter_resolved ~= nil then
+        print("󰦕  Lint launched with : " .. linter_resolved)
+      end
+      lint.try_lint(linter_resolved)
     end, { desc = "Trigger linting for current file." })
 
     vim.keymap.set("n", "<leader>lp", function()
-      print(lint_progress())
+      local running_linters = require("lint").get_running()
+      if #running_linters == 0 then
+        print("󰦕  No linters running.")
+      end
+      print("󱉶  linters running: " .. table.concat(linters, ", "))
     end, { desc = "Get the current running linters for the current buffer" })
   end,
 }
